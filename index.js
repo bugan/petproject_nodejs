3cc7daa6919c508a2034d39a89178ec4ae0f032f
Ricardo <dones.bugan@gmail.com>
2019-09-21 17:57:55 -0300

    adicionando eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+const express = require('express');
+const app = express();
 
-var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Example app listening on port 3000!');
 });
-
